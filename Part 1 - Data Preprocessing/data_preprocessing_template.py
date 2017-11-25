eb79015ea367424f023fcbe1a10bef079ff245b8
minaamfasihi <minaam.fasihi@gmail.com>
2017-11-25 22:19:20 +0500

    Transformed the data using imputer

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -15,4 +15,5 @@ y = dataset.iloc[:, 3].values
 
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer.fit()
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
